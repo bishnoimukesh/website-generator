3e1ff7851ea88f95d70d90c2b24a10cbea42e5b5
bishnoimukesh <mukeshbishnoi@gmail.com>
2025-07-01 23:26:19 +0530

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { Header } from "@/components/ui/navigation-menu";
 import { ApplicationStats } from "@/components/ui/application-stats";
 import { Footer } from "@/components/ui/footer/footer";
 import { IconSection } from "@/components/ui/icon-section";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Analytics />
       <Header />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <HeroPrompt 
